27d5e7ceb4f2d2784ba3b58729384aba9dc2b6d4
trogdoro/ <craig.muth@gmail.com>
2019-12-12 19:53:29 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Xiki package
+
+Search code snippets right from your code comments!
+
+## Features
+
+Not complete yet. Check back soon!
+
+### 0.0.1
+
+Initial placeholder release.
+
---
+++ spec/xiki-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import XikiView from '../lib/xiki-view';
+
+describe('XikiView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
